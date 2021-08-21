04f09a93fca324901a5b0b238890385b9dab4da3
Huynh <conghuynhho12@gmail.com>
2021-08-21 20:01:35 +0700

    add header, welcome and about section

--- cake-store/app.js
+++ cake-store/app.js
@@ -1,1 +1,12 @@
-console.log({cakes})
+function onClickToggle (){
+  // toggle navbar
+  const navbar = document.getElementById('nav');
+  if(navbar){
+    navbar.classList.toggle('open');
+  }
+  // change icon of toggle button
+  const toggleButton = document.getElementById('toggleButton');
+  if(toggleButton){
+    toggleButton.classList.toggle('open')
+  }
+}
