e7516620727acf5f38264a6419255e526ffe4473
Huynh <conghuynhho12@gmail.com>
2021-08-21 10:23:26 +0700

    init project

---
+++ cake-store/cakes.js
@@ -0,0 +1,62 @@
+const cakes = [
+  {
+    name: "Sweet Item",
+    price: "5",
+    image: "static/images/sweets-1.jpeg"
+  },
+  {
+    name: "Cupcake Item",
+    price: "5",
+    image: "static/images/cupcake-1.jpeg"
+  },
+  {
+    name: "Cake Item",
+    price: "5",
+    image: "static/images/cake-1.jpeg"
+  },
+  {
+    name: "Dougnut Item",
+    price: "5",
+    image: "static/images/doughnut-1.jpeg"
+  },
+  {
+    name: "Sweet Item",
+    price: "10",
+    image: "static/images/sweets-2.jpeg"
+  },
+  {
+    name: "Cupcake Item",
+    price: "10",
+    image: "static/images/cupcake-2.jpeg"
+  },
+  {
+    name: "Cake Item",
+    price: "10",
+    image: "static/images/cake-2.jpeg"
+  },
+  {
+    name: "Sweet Item",
+    price: "15",
+    image: "static/images/sweets-3.jpeg"
+  },
+  {
+    name: "Cupcake Item",
+    price: "15",
+    image: "static/images/cupcake-3.jpeg"
+  },
+  {
+    name: "Cake Item",
+    price: "15",
+    image: "static/images/cake-3.jpeg"
+  },
+  {
+    name: "Cupcake Item",
+    price: "5",
+    image: "static/images/cupcake-1.jpeg"
+  },
+  {
+    name: "Cupcake Item",
+    price: "5",
+    image: "static/images/cupcake-1.jpeg"
+  },
+]
