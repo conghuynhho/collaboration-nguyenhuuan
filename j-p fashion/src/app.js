b02736e56ee63f72b75741cd292e416b1320c021
Huynh <conghuynhho12@gmail.com>
2021-08-29 21:09:27 +0700

    commit to test

--- j-p fashion/src/app.js
+++ j-p fashion/src/app.js
@@ -1,2 +1,11 @@
 import data from './store/data.js'
+import { Home } from './screens/Home.js'
+
+const router = () => {
+  console.log('hi')
+  const body = document.getElementById('main-section')
+  body.innerHTML = Home.render() 
+}
+
+window.addEventListener('load', router)
 console.log(data)
